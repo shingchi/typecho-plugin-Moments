a345c052e2f536715542cab8785be416949e6191
shingchi <1904614+shingchi@users.noreply.github.com>
2025-07-06 04:32:10 +0000

    Setup project for typecho-plugin-Moments

--- plugin/Plugin.php
+++ plugin/Plugin.php
@@ -11,12 +11,12 @@ if (!defined('__TYPECHO_ROOT_DIR__')) {
 }
 
 /**
- * %description%
+ * 
  *
- * @package %name%
- * @author %author%
+ * @package typecho-plugin-Moments
+ * @author shingchi
  * @version %version%
- * @link %link%
+ * @link https://github.com/shingchi/typecho-plugin-Moments
  */
 class Plugin implements PluginInterface
 {
